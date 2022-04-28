07e227cf08a314d81e57a42f8f8555312dc8bde0
AzmiHabib <azmihabib0323@gmail.com>
2022-04-29 01:19:54 +0600

    making all routes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,27 +1,71 @@
 import React from "react";
-import './Header.css'
+import "./Header.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+
 
 const Header = () => {
   return (
     <div>
       <Navbar collapseOnSelect expand="lg" bg="info" variant="dark">
         <Container>
-          <NavLink to="/"><img src="" alt="Img hobe" /></NavLink>
+          <NavLink to="/">
+            <img src="" alt="Img hobe" />
+          </NavLink>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-              <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/">Home</NavLink>
-              <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/blogs">Blogs</NavLink>
-              <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/services">Services</NavLink>
-              <NavLink className={({ isActive }) => (isActive ? "link-active" : "link")} to="/about">About</NavLink>
+            <Nav className="me-auto">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/"
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/blogs"
+              >
+                Inventory
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/services"
+              >
+                Contact
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
             </Nav>
             <Nav>
-              <Nav.Link >More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/login"
+              >
+                Login
+              </NavLink>
+
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "link-active" : "link"
+                }
+                to="/signUp"
+              >
+                Sign Up
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
